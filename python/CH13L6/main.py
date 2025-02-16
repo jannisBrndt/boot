8e6b2aa3f62e18b1aec8f1b1f453e5ecc5bc3e41
jannisBrndt <jannis.brandt@alumni.fh-aachen.de>
2025-02-16 14:47:28 +0100

    Corrected chapter 13 lesson 6

--- python/CH13L6/main.py
+++ python/CH13L6/main.py
@@ -1,18 +1,19 @@
 def fizzbuzz(start, end):
     list = []
 
-    for i in range(start, end + 1):
-        if i % 3 == 0:
-            list.append("fizz")
+    for i in range(start, end):
         if i % 3 == 0 and i % 5 == 0:
             list.append("fizzbuzz")
+        elif i % 5 == 0:
+            list.append("buzz")
+        elif i % 3 == 0:
+            list.append("fizz")
         else:
             list.append(i)
     return list
 
 def main():
-    start = 0
-    end = 10
+    start = 5
+    end = 31
     fizzbuzz(start, end)
-
 main()
